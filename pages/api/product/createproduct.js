2e5022a16ff8b77a1024df5a43c19caa4942d493
sabadevs20 <sabadevs20@gmail.com>
2022-02-11 22:25:39 +0300

    frii

--- pages/api/product/createproduct.js
+++ pages/api/product/createproduct.js
@@ -1,29 +1,32 @@
 import excuteQuery from '../../../lib/db'
 
 const handler = async (req, res) => {
-  const { product,productCode,category,vehicleClass,underwriter,coverage,description,clauses,waranty,benefits,
-    excludedVehicles,maxTonnage,minTonnage,weeklyRates,monthlyRates,fortniteRate,passengers,annualRates,maxExcluded,
-    minExcluded,maxAge,minAge,maxInsured,minInsured,minPremium,optionalPremium,optionalName,optionalRate,owner,unique } = req.body
+ const  {product,productCode,category,
+  vehicleClass,underwriter,coverage,
+  description,clauses,waranty,
+  benefits,
+  excludedVehicles,maxTonnage,
+  minTonnage,weeklyRates,monthlyRates,
+  fortniteRate,passengers,annualRates,
+  maxAge,minAge,maxInsured,
+  minInsured,minPremium,optionalPremium,
+  optionalRate,optionalName,
+  owner,months2,
+  months3,months4,
+  months5,months6,months7,
+  months8,months9,months10,months11} =req.body
   try {
-    if (!product||!productCode||!category||!vehicleClass||!underwriter||!coverage||!description||!clauses||!waranty||!benefits
-      ||!excludedVehicles||!maxTonnage||!minTonnage||!weeklyRates||!monthlyRates||!fortniteRate||!passengers||!annualRates||!maxExcluded||
-      !minExcluded||!maxAge||!minAge||!maxInsured||!minInsured||!minPremium||!optionalPremium||!optionalName||!optionalRate||!owner ||!unique) {
-      return res
-        .status(400)
-        .json({ message: '`title` and `content` are both required' })
-    }
+   
 
     const results = await excuteQuery({
       
-      query: 'INSERT INTO `itbl_product` (product_code,category, vehicleclass,underwriter,coverage,description,clauses,conditionsandwaranties,optionalname,optionalpremium,optionalrate,policylimits,maxtonnage,weeklyrates,fortnightrates,monthlyrates,annualrates,excludedvehicles,minimumpremium,passengers,maxage,minage,maxsum,minsum,owner,uniqueidentifier) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)' ,
-      values:  [productCode,category,vehicleClass,underwriter,coverage,description,clauses,waranty,benefits,optionalName,optionalPremium,optionalRate,benefits,
-        maxTonnage,weeklyRates,fortniteRate,monthlyRates,annualRates,excludedVehicles,minPremium,passengers,maxAge,minAge,maxInsured,minExcluded,owner,unique]
      
     })
     console.log(results)
     return res.json(results)
   } catch (e) {
-    res.status(500).json({ message: e.message })
+    console.log(e)
+    res.status(500).json({ message: e })
   }
 }
 
